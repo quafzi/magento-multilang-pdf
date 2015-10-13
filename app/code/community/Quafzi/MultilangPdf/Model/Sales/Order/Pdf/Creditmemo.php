08c8e3bddfae670b339f083cfa00470c55bb1845
Thomas Birke <quafzi@netextreme.de>
2015-10-13 13:14:54 +0200

    Update methods to be compatible to Magento 1.9

--- app/code/community/Quafzi/MultilangPdf/Model/Sales/Order/Pdf/Creditmemo.php
+++ app/code/community/Quafzi/MultilangPdf/Model/Sales/Order/Pdf/Creditmemo.php
@@ -49,51 +49,35 @@ class Quafzi_MultilangPdf_Model_Sales_Order_Pdf_Creditmemo extends Mage_Sales_Mo
         /**
          * The following code is taken unmodified from the base class
          */
-        $page = $pdf->newPage(Zend_Pdf_Page::SIZE_A4);
-        $pdf->pages[] = $page;
-
+        $page  = $this->newPage();
         $order = $creditmemo->getOrder();
-
         /* Add image */
         $this->insertLogo($page, $creditmemo->getStore());
-
         /* Add address */
         $this->insertAddress($page, $creditmemo->getStore());
-
         /* Add head */
-        $this->insertOrder($page, $order, Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_CREDITMEMO_PUT_ORDER_ID, $order->getStoreId()));
-
-        $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
-        $this->_setFontRegular($page);
-        $page->drawText(Mage::helper('sales')->__('Credit Memo # ') . $creditmemo->getIncrementId(), 35, 780, 'UTF-8');
-
+        $this->insertOrder(
+            $page,
+            $order,
+            Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_CREDITMEMO_PUT_ORDER_ID, $order->getStoreId())
+        );
+        /* Add document text and number */
+        $this->insertDocumentNumber(
+            $page,
+            Mage::helper('sales')->__('Credit Memo # ') . $creditmemo->getIncrementId()
+        );
         /* Add table head */
-        $page->setFillColor(new Zend_Pdf_Color_RGB(0.93, 0.92, 0.92));
-        $page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
-        $page->setLineWidth(0.5);
-        $page->drawRectangle(25, $this->y, 570, $this->y-15);
-        $this->y -=10;
-        $page->setFillColor(new Zend_Pdf_Color_RGB(0.4, 0.4, 0.4));
         $this->_drawHeader($page);
-        $this->y -=15;
-
-        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
-
         /* Add body */
         foreach ($creditmemo->getAllItems() as $item){
             if ($item->getOrderItem()->getParentItem()) {
                 continue;
             }
-
-            if ($this->y<20) {
-                $page = $this->newPage(array('table_header' => true));
-            }
-
             /* Draw item */
-            $page = $this->_drawItem($item, $page, $order);
+            $this->_drawItem($item, $page, $order);
+            $page = end($pdf->pages);
         }
-
         /* Add totals */
-        $page = $this->insertTotals($page, $creditmemo);
+        $this->insertTotals($page, $creditmemo);
     }
 }
